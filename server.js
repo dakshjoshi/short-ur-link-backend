ce02cfd15d233690a3b18eba701033e1ea0af724
dakshjoshi <64137744+dakshjoshi@users.noreply.github.com>
2021-04-13 08:57:04 +0530

    Update server.js
    Adding registration and login functionalities, next comes JWT tokens

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongodb = require("mongodb");
-
+const bcrypt = require("bcrypt");
 const URL = "mongodb+srv://Daksh:Daksh@08@cluster0.psgym.mongodb.net/test";
 //How to secure the link ?
 const DB = "users";
@@ -23,13 +23,25 @@ app.use(express.json());
 // 3. Making everything operational from frontend instead of from Insomnia --DONE
 
 //Things need to be done :
-//1. Upload to cloud and adjust links accordingly -- IN PROGRESS
+//1. Upload to cloud and adjust links accordingly -- DONE
 //2. Make UI very beautiful/sexy, include form input required and parameters
 //3. Understand your own code
 //4. Add lazy loading to pages
 
+//Things to do for 12/04/2021 night
+//1. Create working register and login page
+//2. Find method to re-direct user after registering to his workspace
+//3. Make website operational on netlify
+
 app.post("/userList", async function (req, res) {
   try {
+    //Checking for already existing username
+
+    //Hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+
     //connect to db server
     let connection = await mongodb.connect(URL);
     //Select the db
@@ -44,7 +56,7 @@ app.post("/userList", async function (req, res) {
     //disconnect from db
     await connection.close();
 
-    res.json("data recieved");
+    res.json({ message: "User Added" });
   } catch (error) {
     console.log(error);
   }
@@ -229,7 +241,27 @@ app.put("/workSpace/:id", async function (req, res) {
   }
 });
 
-app.post("/directTo/:id", function (req, res) {});
-app.get("/directTo/:id", function (req, res) {});
+app.get("/login", async function (req, res) {
+  try {
+    //Connecting to DB
+    let connection = await mongodb.connect(URL);
+    let db = connection.db(DB);
+    let user = await db
+      .collection("userList")
+      .findOne({ user_id: req.body.user_id });
+    await connection.close();
+
+    //Comparing the Hash
+    let hashCompare = bcrypt.compareSync(req.body.password, user.password);
+
+    if (hashCompare == true) {
+      res.send({ message: "hash is true" });
+    } else {
+      res.send({ message: "hash is not true" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(process.env.PORT || 6969);
